3aa51d6f1b682ad17d302afddecc3447070edfa0
Vladimir Yudintsev <git@yv84.me>
2015-02-24 17:57:14 +0500

    initial commit

---
+++ src/main/java/me/yv84/springlayout/App.java
@@ -0,0 +1,13 @@
+package me.yv84.springlayout;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
