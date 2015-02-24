a3fac90b2e3cde9e61533b02531b5778676be0eb
Vladimir Yudintsev <git@yv84.me>
2015-02-24 22:26:13 +0500

    slf4j+log4j support

--- src/main/java/me/yv84/springlayout/controller/IndexController.java
+++ src/main/java/me/yv84/springlayout/controller/IndexController.java
@@ -1,5 +1,7 @@
 package me.yv84.springlayout.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +13,17 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class IndexController {
+    static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @RequestMapping("/")
     public ModelAndView index() {
+        logger.info("index.page");
         return new ModelAndView("index");
     }
 
     @RequestMapping("/**")
     public ModelAndView pageNotFound() {
+        logger.warn("404.page");
         return new ModelAndView("404");
     }
 }
